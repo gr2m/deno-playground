f8065d731d95d9a24311663e91a0c6ba052c3128
Gregor Martynus <gregor@martynus.net>
2020-05-12 14:24:04 -0700

    fix: publish contents of `pkg/` folder

--- octokit-request.ts
+++ octokit-request.ts
@@ -1,3 +1,7 @@
-import { request } from "https://cdn.pika.dev/@octokit/request";
-console.log("ok");
-console.log(await request("/"));
+// import { request } from "https://cdn.pika.dev/@octokit/request";
+// console.log("ok");
+// console.log(await request("/"));
+
+import { VERSION } from "https://cdn.pika.dev/@octokit/types@2.16.1";
+console.log(`VERSION`);
+console.log(VERSION);
