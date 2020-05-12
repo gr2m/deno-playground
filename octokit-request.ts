ee19fb1385ce166903c09f57620fe715a7b329c6
Gregor Martynus <gregor@martynus.net>
2020-05-12 15:04:28 -0700

    fix: reset octokit-request.ts script (oopsie)

--- octokit-request.ts
+++ octokit-request.ts
@@ -1,7 +1,3 @@
-// import { request } from "https://cdn.pika.dev/@octokit/request";
-// console.log("ok");
-// console.log(await request("/"));
-
-import { VERSION } from "https://cdn.pika.dev/@octokit/types@2.16.1";
-console.log(`VERSION`);
-console.log(VERSION);
+import { request } from "https://cdn.pika.dev/@octokit/request";
+console.log("ok");
+console.log(await request("/"));
